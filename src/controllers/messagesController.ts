53509406b1e7153577135b30033d26b5396cf425
ahammedalip <ahammed.puthuppalli@gmail.com>
2024-03-15 17:17:23 +0530

    get mesage for conversation is changed

--- src/controllers/messagesController.ts
+++ src/controllers/messagesController.ts
@@ -1,5 +1,6 @@
 import { Request,Response } from "express";
 import { Messages } from "../models/Messages";
+import { Conversation } from "../models/Conversations";
 
 
 export const addMessage = async (req: Request, res: Response)=>{
@@ -15,15 +16,31 @@ export const addMessage = async (req: Request, res: Response)=>{
 }
 
 export const getMessage = async (req: Request, res: Response) => {
+
+    console.log('coming here to get message');
+    console.log('query id converstionId',req.query);
  
     try {
+       
+        const senderId = req.query.sender;
+        const recipientId = req.query.recipient;
+
+        // Query to find a conversation that includes both the sender and recipient IDs in its members array
+        const conversation = await Conversation.findOne({
+            members: { $all: [senderId, recipientId] }
+        });
+
+            console.log('conversation id---',conversation._id)
+   
         const messages = await Messages.find({
-            conversationId: req.query.conversationId
+            conversationId: conversation._id
         })    
-        console.log(messages);
-        res.status(200).json({success:true, messages})
+        console.log('messages of given conversation',messages);
+        res.status(200).json({success:true, messages, conversationId: conversation?._id})
     } catch (error) {
         console.log('error at getting messages', error);
         res.status(500).json({success:false, message: 'error while fetching message'})
     }
 }
+
+
