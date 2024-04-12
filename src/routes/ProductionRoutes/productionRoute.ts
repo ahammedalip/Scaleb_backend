a0cd10162c3a6f5c36c77b955f99999ec808080f
ahammedalip <ahammed.puthuppalli@gmail.com>
2024-04-12 11:13:06 +0530

    added some more routes

--- src/routes/ProductionRoutes/productionRoute.ts
+++ src/routes/ProductionRoutes/productionRoute.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { verifyProduction } from '../../utils/verifyUser';
-import {  acceptReq, addItem, addSubscription, availableSales, fetchRequestedRetailers, 
-    getAvailRetailList, getConnRetailersList, getProfile, getRetailerProfile, getSalesProfile, 
-     rejectReq, searchRetailer, sendConnectionRequest, 
-     sortRetailer} from '../../controllers/ProductionController/ProductionController';
-import { acceptOrder, fetchOrdersAll, rejectOrder } from '../../controllers/ProductionController/fetchOrder';
+import {
+    acceptReq, addItem, addSubscription, availableSales, fetchRequestedRetailers,
+    getAvailRetailList, getConnRetailersList, getProfile, getReports, getRetailerProfile, getSalesProfile,
+    rejectReq, reports, searchRetailer, sendConnectionRequest,
+    sortRetailer
+} from '../../controllers/ProductionController/ProductionController';
+import { acceptOrder, countOrder, fetchOrdersAll, rejectOrder } from '../../controllers/ProductionController/fetchOrder';
 
 
 
@@ -17,18 +19,19 @@ router.post('/addItem', verifyProduction, addItem)
 router.get('/requests', verifyProduction, fetchRequestedRetailers)
 router.delete('/delete-req', verifyProduction, rejectReq)
 router.post('/acc-req', verifyProduction, acceptReq)
-router.get('/orders', verifyProduction,fetchOrdersAll)
-router.patch('/order-acc', verifyProduction,acceptOrder)
-router.patch('/order-rej', verifyProduction,rejectOrder)
-router.get('/available-sales', verifyProduction,availableSales)
+router.get('/orders', verifyProduction, fetchOrdersAll)
+router.get('/order-count', verifyProduction, countOrder)
+router.patch('/order-acc', verifyProduction, acceptOrder)
+router.patch('/order-rej', verifyProduction, rejectOrder)
+router.get('/available-sales', verifyProduction, availableSales)
 router.post('/sales-prof', verifyProduction, getSalesProfile)
 router.get('/conn-ret', verifyProduction, getConnRetailersList)
 router.get('/avail-ret', verifyProduction, getAvailRetailList)
-router.get('/ret-profile', verifyProduction,getRetailerProfile)
+router.get('/ret-profile', verifyProduction, getRetailerProfile)
 router.patch('/conn-req', verifyProduction, sendConnectionRequest)
-router.patch('/subscription', verifyProduction,addSubscription)
-router.get('/search-user', verifyProduction,searchRetailer)
+router.patch('/subscription', verifyProduction, addSubscription)
+router.get('/search-user', verifyProduction, searchRetailer)
 router.get('/retailer-sort', verifyProduction, sortRetailer)
-
+router.get('/report', verifyProduction,getReports)
 
 export default router;
